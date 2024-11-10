9b52e1b30f4335af12c95716eb7bde09fd284584
metaworkzdevteam <metaworkzteam@gmail.com>
2024-11-10 15:40:32 +0530

    Initial commit 🚀

---
+++ packages/animateX/vite.config.ts
@@ -0,0 +1,27 @@
+/// <reference types="vite/client" />
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import dts from 'vite-plugin-dts'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), libInjectCss(), dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "React-dom",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+      },
+    },
+  },
+});
